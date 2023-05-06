5fb08414de4ac07d229932c8e825e8aa21bd7ca7
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-06 17:14:04 +0300

    Implement database calls in the frontend app
    Replace the "stub" implementation with HTTP requests to the database
    service implemented with the backend app.

--- frontend/frontendapp/database.py
+++ frontend/frontendapp/database.py
@@ -1,29 +1,91 @@
+import logging
+import os
+import time
+
+import requests
+
+
+_GENRES_URL = f"{os.environ['DB_SERVICE']}/movies/genres"
+
+_MOVIES_LISTING_URL = f"{os.environ['DB_SERVICE']}/movies/all"
+
+_MAX_RETRIES = 4
+
 
 def list_movie_genres():
+    """
+    :returns: a list of strs - all the genres of the available movies.
+    """
+    return _do_request(_GENRES_URL, default=[])
+
+
+def _do_request(url, params=None, default=None):
+    for i in range(1, _MAX_RETRIES + 1):
+        response = requests.get(url, params)
+
+        if (response.status_code == requests.codes.ok):
+            return response.json()
+        else:
+            logging.error(
+                f"'{url}' returned {response.status_code} at "
+                f"try {i}: {response.text}"
+            )
+            time.sleep(2 << i)
+
+    return default
+
+
+def list_movies(title=None,
+                genres=None,
+                length=None,
+                year=None):
+    """
+    Lists movies that conform to the criteria defined with the optional
+    arguments described below.
+
+    :param title: a str - a title to match.
+    :param genres: a list of strs - allowed genres.
+    :param length: a two-element list of ints representing an interval
+    [a, b] of allowed movie length (in minutes).
+    :param year: a two-element list of ints representing an interval
+    [a, b] of allowed movie year.
+    :returns: a list of movie documents (dicts).
+    """
+    params = _params_for(title, genres, length, year)
+    movies = _do_request(_MOVIES_LISTING_URL, params, default=[])
+
+    return _transform(movies)
+
+
+def _params_for(title, genres, length, year):
+    params = {}
+
+    if (title is not None):
+        params["title"] = title
+
+    if (genres is not None):
+        params["genres"] = genres
+
+    if (length is not None):
+        params["length"] = length
+
+    if (year is not None):
+        params["year"] = year
+
+    return params
+
+
+def _transform(movies):
     return [
-        "Comedy",
-        "Drama",
-        "Thriller",
+        _transform_fields_of(m)
+        for m in movies
     ]
 
 
-def list_movies(title, genres, length, year):
-    return [{
-        "title": "Titanic",
-        "year": 1997,
-        "length": 194,
-        "rating": "7.7 (716392)",
-        "id": 0,
-        "image_url": r"https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SY1000_SX677_AL_.jpg",
-        "plot": "A seventeen-year-old aristocrat falls in love with a kind, but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
-        "cast": ", ".join([
-           "Leonardo DiCaprio",
-           "Kate Winslet",
-           "Billy Zane",
-           "Kathy Bates"
-        ]),
-        "genre": ", ".join([
-            "Drama",
-            "Romance"
-        ]),
-    }]
+def _transform_fields_of(m):
+    imdb = m.pop("imdb")
+    m["rating"] = f"{imdb['rating']} ({imdb['votes']})"
+    m["genre"] = ", ".join(m["genre"])
+    m["cast"] = ", ".join(m["cast"])
+
+    return m
