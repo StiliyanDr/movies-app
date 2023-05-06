e3d84e59e00dcf428714b53d0a844938b4222715
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-06 15:52:21 +0300

    Add scripts for running the backend app

---
+++ backend/run.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=backendapp
+set FLASK_ENV=development
+set MONGO_URI=mongodb+srv://stiliyandragnev:<PASS>@cluster0.cfp0ybs.mongodb.net/?retryWrites=true&w=majority
+python -m flask run --debug
