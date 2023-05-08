510a87a2357e653c92cc293472482c7c4e91a7aa
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-08 11:47:20 +0300

    Update the frontend app's run file
    Make it configurable through env vars for debug mode, host name and
    port.

--- frontend/run.py
+++ frontend/run.py
@@ -1,7 +1,12 @@
+import os
+
 from frontendapp.app import create_app
 
 
 if __name__ == "__main__":
+    do_debug = os.environ.get("DEBUG").lower() == "true"
+    host = os.environ.get("DASH_HOST", "localhost")
+    port = int(os.environ.get("DASH_PORT", 8080))
+
     app = create_app()
-    app.run_server(debug=True,
-                   port=8080)
+    app.run_server(debug=do_debug, host=host, port=port)
