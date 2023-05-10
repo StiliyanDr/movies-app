1639b65c8b883f34da2f2a69a45663ae9232af6d
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-10 17:20:41 +0300

    Make the frontend handle comments
    Add callbacks that update the comments section and submit comments

--- frontend/frontendapp/callbacks.py
+++ frontend/frontendapp/callbacks.py
@@ -4,6 +4,7 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from frontendapp import database as db
+from frontendapp.layout import comment_layout_for
 
 
 def define_callbacks_for(app):
@@ -69,5 +70,60 @@ def define_callbacks_for(app):
     )
     def toggle_movie_modal(title, close_clicks):
         return ctx.triggered_id == "movie-title"
+
+    @app.callback(
+        Output("comments-list-group", "children"),
+        Input("view-button", "n_clicks"),
+        Input("submit-comment-button", "n_clicks"),
+        State("movies-table", "data"),
+        State("movies-table", "selected_rows"),
+        State("comments-list-group", "children"),
+        State("username-input", "value"),
+        State("comment-textarea", "value")
+    )
+    def update_comments(view_clicks,
+                        submit_clicks,
+                        movies,
+                        selected_movie,
+                        current_comments,
+                        username,
+                        new_comment):
+        if (ctx.triggered_id == "view-button" and selected_movie):
+            m = movies[selected_movie[0]]
+            movie_comments = db.list_comments_for(m["_id"])
+
+            return [
+                comment_layout_for(c["name"], c["text"], c["date"])
+                for c in movie_comments
+            ]
+        elif (ctx.triggered_id == "submit-comment-button" and
+              username and
+              new_comment):
+            m = movies[selected_movie[0]]
+            comment_date = db.submit_comment(username,
+                                             new_comment,
+                                             m["_id"])
+
+            if (comment_date is not None):
+                current_comments.append(
+                    comment_layout_for(username,
+                                       new_comment,
+                                       comment_date)
+                )
+
+            return current_comments
+        else:
+            return dash.no_update
+
+    @app.callback(
+        Output("username-input", "value"),
+        Output("comment-textarea", "value"),
+        Input("comments-list-group", "children")
+    )
+    def reset_comment_input(comments):
+        if (ctx.triggered_id is not None):
+            return ("", "")
+        else:
+            raise PreventUpdate()
 
     return app
