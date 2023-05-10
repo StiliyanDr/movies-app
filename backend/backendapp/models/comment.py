c5ac760307427c651b222b25b225cc63bfb7c385
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-10 12:21:00 +0300

    Add the Comment model
    It also uses a PydanticObjectId model from
    https://github.com/roman-right/beanie

---
+++ backend/backendapp/models/comment.py
@@ -0,0 +1,29 @@
+import datetime as dt
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from backendapp.models.objectid import PydanticObjectId
+
+
+class Comment(BaseModel):
+    """
+    Represents a movie comment.
+    """
+    name: str
+    text: str
+    movie_id: PydanticObjectId
+    date: dt.datetime
+    id: Optional[PydanticObjectId] = Field(None, alias="_id")
+
+    def to_bson(self):
+        return self.dict(by_alias=True, exclude_none=True)
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "text": self.text,
+            "date": str(self.date),
+            "movie_id": str(self.movie_id),
+            "_id": str(self.id),
+        }
