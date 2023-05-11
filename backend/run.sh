33761f5e04b8650ac4dcdca442c6998758dc9145
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-05-11 10:18:29 +0300

    Fix a script

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
-export FLASK_APP=flaskr
+export FLASK_APP=backendapp
 export FLASK_ENV=development
 export MONGO_URI='mongodb+srv://stiliyandragnev:<PASS>@cluster0.cfp0ybs.mongodb.net/?retryWrites=true&w=majority'
 python -m flask run --debug
